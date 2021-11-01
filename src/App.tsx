58d497792cad881daaeb9ed3ae04f0dd0f54a0d4
Jeeyoung Han <64334381+hancandice@users.noreply.github.com>
2021-11-01 15:50:30 +0900

    타입스크립트와 useEffect 사용하기

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import MyForm from "./MyForm";
+
+const App: React.FC = () => {
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  };
+  return <MyForm onSubmit={onSubmit} />;
+};
+
+export default App;
